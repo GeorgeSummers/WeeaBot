4fbe9c951be56a9823cc935fff17a94a8286ea7e
GeorgeSummers <tankist1121@gmail.com>
2020-03-18 14:45:56 +0300

    rss

--- rss.py
+++ rss.py
@@ -1,4 +1,7 @@
 import requests
 import xml.etree.ElementTree as et
 import queue
-
+#try feedparser
+#https://stackoverflow.com/questions/22211795/python-feedparser-how-can-i-check-for-new-rss-data
+def listen():
+    pass
